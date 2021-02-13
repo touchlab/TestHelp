8593862c8c2c6758eb75f007c88fa20c43285741
Kevin Galligan <kgalligan@gmail.com>
2021-02-13 18:19:33 -0500

    Try removing posix

--- src/nativeDarwinMain/kotlin/co/touchlab/testhelp/concurrency/sleep.kt
+++ src/nativeDarwinMain/kotlin/co/touchlab/testhelp/concurrency/sleep.kt
@@ -1,7 +1,7 @@
 package co.touchlab.testhelp.concurrency
 
-import platform.posix.usleep
+// import platform.posix.usleep
 
 actual fun sleep(time: Long) {
-    usleep((time * 1000).toUInt()) // this takes microseconds, so multiply by 1000 to use miliseconds.
+    // usleep((time * 1000).toUInt()) // this takes microseconds, so multiply by 1000 to use miliseconds.
 }
