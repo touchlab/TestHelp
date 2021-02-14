6fd014788a467cecc87ea389c2fb3ba24497b680
Kevin Galligan <kgalligan@gmail.com>
2021-02-13 22:19:38 -0500

    No posix

--- src/nativeLinuxMain/kotlin/co/touchlab/testhelp/concurrency/sleep.kt
+++ src/nativeLinuxMain/kotlin/co/touchlab/testhelp/concurrency/sleep.kt
@@ -3,5 +3,9 @@ package co.touchlab.testhelp.concurrency
 // import platform.posix.usleep
 
 actual fun sleep(time: Long) {
+    val start = currentTimeMillis()
+
+    while ((currentTimeMillis() - start) < time) {
+    }
     // usleep((time * 1000).toUInt()) // this takes microseconds, so multiply by 1000 to use miliseconds.
 }
