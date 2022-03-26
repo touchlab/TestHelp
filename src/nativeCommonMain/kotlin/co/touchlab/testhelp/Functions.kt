6ad3b15c3ffd16a78abfc29b259cfeb8b6c100cd
Kevin Galligan <kgalligan@gmail.com>
2022-03-26 13:07:30 -0400

    New memory model. Only freeze if you need to

--- src/nativeCommonMain/kotlin/co/touchlab/testhelp/Functions.kt
+++ src/nativeCommonMain/kotlin/co/touchlab/testhelp/Functions.kt
@@ -3,7 +3,11 @@ package co.touchlab.testhelp
 import kotlin.native.concurrent.freeze
 import kotlin.native.concurrent.isFrozen
 
-actual fun <T> T.freeze(): T = this.freeze()
+actual fun <T> T.freeze(): T = if (Platform.memoryModel == MemoryModel.STRICT) {
+    this.freeze()
+} else {
+    this
+}
 actual val isNative: Boolean = true
 actual val isMultithreaded: Boolean = true
 actual val <T> T.isFrozen: Boolean
